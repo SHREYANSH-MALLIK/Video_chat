ef566a727fc6b7b5f9ef53116313344f90e7f349
SHREYANSH-MALLIK <86109327+SHREYANSH-MALLIK@users.noreply.github.com>
2024-04-22 17:26:16 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express();
 const server = require("http").Server(app);
 app.set("view engine", "ejs");
 app.use(express.static("public"));
-app.use(express.json())
+app.use(express.json());
 
 const { v4: uuidv4 } = require("uuid");
 
@@ -20,6 +20,17 @@ const peerServer = ExpressPeerServer(server, {
 
 app.use("/peerjs", peerServer);
 
+var nodeMailer = require("nodemailer")
+const transporter = nodeMailer.createTransport({
+    port : 465,
+    host : "smtp.gmail.com",
+    auth : {
+        user : "shreyanshmallik@gmail.com",
+        pass : "urks tpdp yytk tcdw"
+    },
+    secure : true
+})
+
 app.get("/", (req, res) => {
     res.redirect(`/${uuidv4()}`);
 });
@@ -28,11 +39,35 @@ app.get("/:room", (req, res) => {
     res.render("index", { roomId: req.params.room });
 });
 
-io.on("connection", (socket) => {
-    socket.on("join-room", (roomId, userId) => {
-        socket.join(roomId);
-        io.to(roomId).emit("user-connected", userId);
+app.post("/send-email",(req,res) => {
+    const to = req.body.to;
+    const url = req.body.url;
+
+    const mailData = {
+        from : "shreyanshmallik@gmail.com",
+        to : to,
+        subject : "Join the video chat with me",
+        html : `<p>Hey there,</p><p>come and join me for a video chat here - ${url}</p>`
+    }
+
+    transporter.sendMail(mailData,(error,info) => {
+        if(error){
+            console.log(error)
+        }
+        else {
+            res.status(200).send({message : "invitation sent", message_id : info.messageId})
+        }
     })
+})
+
+io.on("connection", (socket) => {
+    socket.on("join-room", (roomId, userId, userName) => {
+        socket.join(roomId);
+        io.to(roomId).emit("user-connected" , userId)
+        socket.on("message", (message) => {
+            io.to(roomId).emit("createMessage", message, userName);
+        });
+    });
 });
 
 server.listen(process.env.PORT || 3030);
